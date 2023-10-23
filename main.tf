c2efa349864f6845eb447888fde6380778ede064
oigen181 <126493876+Oigen181@users.noreply.github.com>
2023-10-23 19:27:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,4 +95,93 @@ resource "yandex_vpc_subnet" "subnet-2" {
   network_id     = "${yandex_vpc_network.test-2.id}"
 }
 
+resource "yandex_alb_target_group" "foo" {
+  name           = "targetgroup"
 
+  target {
+    subnet_id    = "${yandex_vpc_subnet.subnet-1.id}"
+    ip_address   = "${yandex_compute_instance.vm-1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id    = "${yandex_vpc_subnet.subnet-2.id}"
+    ip_address   = "${yandex_compute_instance.vm-2.network_interface.0.ip_address}"
+  }
+}
+
+resource "yandex_alb_backend_group" "test-backend-group" {
+  name                     = "backendgroup"
+  session_affinity {
+    connection {
+      source_ip = true
+    }
+  }
+
+  http_backend {
+    name                   = "back1"
+    weight                 = 1
+    port                   = 80
+    target_group_ids       = ["${yandex_alb_target_group.foo.id}"]
+    load_balancing_config {
+      panic_threshold      = 90
+    }
+    healthcheck {
+      timeout              = "10s"
+      interval             = "2s"
+      healthy_threshold    = 10
+      unhealthy_threshold  = 15
+      http_healthcheck {
+        path               = "/"
+      }
+    }
+  }
+}
+
+
+resource "yandex_alb_http_router" "router-1" {
+  name          = "router-1"
+}
+
+resource "yandex_alb_virtual_host" "vh-1" {
+  name                    = "vh-1"
+  http_router_id          = yandex_alb_http_router.router-1.id
+  route {
+    name                  = "route-1"
+    http_route {
+      http_route_action {
+        backend_group_id  = yandex_alb_backend_group.bg-1.id
+        timeout           = "60s"
+      }
+    }
+  }
+}  
+
+
+resource "yandex_alb_load_balancer" "alb-1" {
+  name        = "alb-1"
+  network_id  = yandex_vpc_network.network-1.id
+
+  allocation_policy {
+    location {
+      zone_id   = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.subnet-2.id 
+    }
+  }
+
+  listener {
+    name = "listener-1"
+    endpoint {
+      address {
+        external_ipv4_address {
+        }
+      }
+      ports = [ 80 ]
+    }
+    http {
+      handler {
+        http_router_id = yandex_alb_http_router.router-1.id
+      }
+    }
+  }
+    security_group_ids = [ yandex_vpc_security_group.alb-sg.id, yandex_vpc_security_group.egress-sg.id ]
+}
