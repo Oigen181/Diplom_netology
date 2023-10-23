c57421502621fb2a21c2ad2f23f0c7dcb8ccf079
oigen181 <126493876+Oigen181@users.noreply.github.com>
2023-10-23 16:04:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,89 @@ terraform {
 
 provider "yandex" {
   zone = "ru-central1-a"
-  token = var.yc_token #from ./token.tf
+  token = "y0_AgAAAAACsMP5AATuwQAAAADck-dAeRqPwqvKSoiWy1QzA3Lw6QDapfY"
   cloud_id = "b1gg2hr8eb22j7ht320k"
   folder_id = "b1g4h7anelqa7bko9vvb"
 }
+
+
+resource "yandex_compute_instance" "vm-1" {
+
+  name                      = "linux-vm1"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v3"
+  zone                      = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "d826honb8s0i1jtt6cg"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yaml")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = "${yandex_vpc_network.network-1.id}"
+}
+
+resource "yandex_compute_instance" "vm-2" {
+
+  name                      = "linux-vm1"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v3"
+  zone                      = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "d826honb8s0i1jtt6cg"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-2.id}"
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yaml")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-2" {
+  name = "network-1"
+}
+
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = "${yandex_vpc_network.network-1.id}"
+}
+
