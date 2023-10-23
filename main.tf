8cd99cb0db6dcbf47edc352188c331e061aa4c96
oigen181 <126493876+Oigen181@users.noreply.github.com>
2023-10-23 21:02:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,13 +99,13 @@ resource "yandex_alb_target_group" "foo" {
   name           = "targetgroup"
 
   target {
-    subnet_id    = "${yandex_vpc_subnet.subnet-1.id}"
-    ip_address   = "${yandex_compute_instance.vm-1.network_interface.0.ip_address}"
+    subnet_id    = yandex_vpc_subnet.subnet-1.id
+    ip_address   = yandex_compute_instance.vm-1.network_interface.0.ip_address
   }
 
   target {
-    subnet_id    = "${yandex_vpc_subnet.subnet-2.id}"
-    ip_address   = "${yandex_compute_instance.vm-2.network_interface.0.ip_address}"
+    subnet_id    = yandex_vpc_subnet.subnet-2.id
+    ip_address   = yandex_compute_instance.vm-2.network_interface.0.ip_address
   }
 }
 
@@ -142,14 +142,14 @@ resource "yandex_alb_http_router" "router-1" {
   name          = "router-1"
 }
 
-resource "yandex_alb_virtual_host" "vh-1" {
+resource "yandex_alb_virtual_host" "virthost" {
   name                    = "vh-1"
   http_router_id          = yandex_alb_http_router.router-1.id
   route {
     name                  = "route-1"
     http_route {
       http_route_action {
-        backend_group_id  = yandex_alb_backend_group.bg-1.id
+        backend_group_id  = "{yandex_alb_backend_group.back1.id}"
         timeout           = "60s"
       }
     }
@@ -157,14 +157,19 @@ resource "yandex_alb_virtual_host" "vh-1" {
 }  
 
 
-resource "yandex_alb_load_balancer" "alb-1" {
+resource "yandex_alb_load_balancer" "balancertest" {
   name        = "alb-1"
-  network_id  = yandex_vpc_network.network-1.id
+  network_id  = "{yandex_vpc_network.network-1.id}"
 
   allocation_policy {
     location {
       zone_id   = "ru-central1-a"
-      subnet_id = yandex_vpc_subnet.subnet-2.id 
+      subnet_id = "{yandex_vpc_subnet.subnet-1.id}" 
+    }
+    
+    location {
+      zone_id   = "ru-central1-b"
+      subnet_id = "{yandex_vpc_subnet.subnet-2.id}"
     }
   }
 
@@ -179,9 +184,8 @@ resource "yandex_alb_load_balancer" "alb-1" {
     }
     http {
       handler {
-        http_router_id = yandex_alb_http_router.router-1.id
+        http_router_id = "{yandex_alb_http_router.router-1.id}"
       }
     }
   }
-    security_group_ids = [ yandex_vpc_security_group.alb-sg.id, yandex_vpc_security_group.egress-sg.id ]
 }
